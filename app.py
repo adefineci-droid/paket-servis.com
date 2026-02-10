085467ab2c4a6297237e0cbb8181af1eee686897
adefineci-droid <adefineci@gmail.com>
2026-02-10 15:18:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# static_folder='static' ekleyerek Flask'a yerini kesin olarak söylüyoruz
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
