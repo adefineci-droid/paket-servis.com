cba8eafcb9f3c28298ad8a49e3e48dea5a627874
adefineci-droid <adefineci@gmail.com>
2026-02-10 15:26:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 from flask import Flask, render_template
 
-# static_folder='static' ekleyerek Flask'a yerini kesin olarak söylüyoruz
-app = Flask(__name__, static_folder='static')
+# Dosya yollarını işletim sistemine göre otomatik ayarlar
+app = Flask(__name__, 
+            static_url_path='/static', 
+            static_folder='static')
 
 @app.route('/')
 def home():
+    # Templates klasöründeki index.html'i çağırır
     return render_template('index.html')
 
 if __name__ == '__main__':
